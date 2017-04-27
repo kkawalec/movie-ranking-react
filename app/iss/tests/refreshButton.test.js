4aa72e7aca174ec9096cd38d869210c06d34ca58
Aedriel <aedriell@gmail.com>
2017-04-27 22:18:15 +0200

    adding test redresh button component

--- app/iss/tests/refreshButton.test.js
+++ app/iss/tests/refreshButton.test.js
@@ -1,15 +1,38 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 import RefreshButton from '../RefreshButton'
 
 const props = {
   handleRefresh: jest.fn(),
-  isLoading: false,
+  isLoading: true,
 }
 
-describe('RefreshButton', () => {
-  it('should render', () => {
-    const wrapper = shallow(<RefreshButton {...props} />)
-    expect(wrapper).toBeDefined()
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
+const shallowComponent = mount(
+  <MuiThemeProvider>
+    <RefreshButton {...props} />
+  </MuiThemeProvider>,
+)
+
+describe('<RefreshButton />', () => {
+  it('Should contains refresh text', () => {
+    expect(shallowComponent.find('span').text()).toBe('Refresh')
+  })
+
+  it('Should be disabled when loading', () => {
+    const button = shallowComponent.find(RefreshButton)
+    expect(button.props().isLoading).toBe(true);
+    expect(button.find('button').html()).toContain('disabled=""')
+  })
+
+  it('Should have handleRefresh function prop', () => {
+    const button = shallowComponent.find(RefreshButton)
+    expect(button.props().handleRefresh).toBe(props.handleRefresh)
   })
 })
